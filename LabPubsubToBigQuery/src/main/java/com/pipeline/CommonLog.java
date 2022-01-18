0327d934af7b7ddace9833c920fa8ed90aeb6de3
MrMime4 <manthanthadwe19499@gmail.com>
2022-01-18 11:26:43 +0530

    pipeline code on branch pipeline updated

--- LabPubsubToBigQuery/src/main/java/com/pipeline/CommonLog.java
+++ LabPubsubToBigQuery/src/main/java/com/pipeline/CommonLog.java
@@ -1,2 +1,21 @@
-package com.pipeline;public class CommonLog {
+package com.pipeline;
+
+import org.apache.beam.sdk.schemas.JavaFieldSchema;
+import org.apache.beam.sdk.schemas.annotations.DefaultSchema;
+
+import javax.annotation.Nullable;
+
+@DefaultSchema(JavaFieldSchema.class)
+public class CommonLog {
+    int id;
+    @javax.annotation.Nullable
+    String name;
+    @javax.annotation.Nullable
+    String surname;
+
+    public CommonLog() {
+        this.id = 0;
+        this.name = "default-name";
+        this.surname = "default-surname";
+    }
 }
--- LabPubsubToBigQuery/src/main/java/com/pipeline/MyPipeline.java
+++ LabPubsubToBigQuery/src/main/java/com/pipeline/MyPipeline.java
@@ -15,20 +15,19 @@ import org.apache.beam.sdk.schemas.Schema;
 import org.apache.beam.sdk.transforms.DoFn;
 import org.apache.beam.sdk.transforms.PTransform;
 import org.apache.beam.sdk.transforms.ParDo;
-import org.apache.beam.sdk.transforms.windowing.FixedWindows;
-import org.apache.beam.sdk.transforms.windowing.Window;
 import org.apache.beam.sdk.values.*;
 
-import org.joda.time.Duration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 
 public class MyPipeline {
 
-    static final TupleTag<bQTableSchema> VALID_Messages = new TupleTag<bQTableSchema>() {
+    static final TupleTag<CommonLog> VALID_Messages = new TupleTag<>() {
     };
-    static final TupleTag<String> INVALID_Messages = new TupleTag<String>() {
+//    static final TupleTag<String> VALID_Messages = new TupleTag<>() {
+//    };
+    static final TupleTag<String> INVALID_Messages = new TupleTag<>() {
     };
 
     private static final Logger LOG = LoggerFactory.getLogger(MyPipeline.class);
@@ -54,19 +53,20 @@ public class MyPipeline {
     }
 
 
-    static class CommonLog extends PTransform<PCollection<String>, PCollectionTuple> {
+    static class JsonToCommonLog extends PTransform<PCollection<String>, PCollectionTuple> {
         @Override
         public PCollectionTuple expand(PCollection<String> input) {
             return input
-                    .apply("JsonToCommonLog", ParDo.of(new DoFn<String, bQTableSchema>() {
+                    .apply("JsonToCommonLog", ParDo.of(new DoFn<String, CommonLog>() {
                                 @ProcessElement
                                 public void processElement(ProcessContext context) {
                                     String json = context.element();
                                     Gson gson = new Gson();
                                     try {
-                                        bQTableSchema commonLog = gson.fromJson(json, bQTableSchema.class);
+                                        CommonLog commonLog = gson.fromJson(json, CommonLog.class);
                                         context.output(VALID_Messages, commonLog);
-                                    } catch (JsonSyntaxException e) {
+                                    } catch (Exception e) {
+                                        LOG.info(e.toString());
                                         context.output(INVALID_Messages, json);
                                     }
                                 }
@@ -75,17 +75,53 @@ public class MyPipeline {
         }
     }
 
-    static public class ConvertToTableRow extends DoFn<bQTableSchema, TableRow> {
-        @ProcessElement
-        public void processing(ProcessContext context) {
-            TableRow tableRow = new TableRow()
-                    .set("id", context.element())
-                    .set("name", context.element())
-                    .set("surname", context.element());
+//    static class JsonToCommonLog extends DoFn<String, String> {
+//        @ProcessElement
+//        public void processElement(@Element String json,ProcessContext processContext) throws Exception {
+//            // try {
+//            //     Gson gson = new Gson();
+//            //     CommonLog commonLog = gson.fromJson(json, CommonLog.class);
+//            //     processContext.output(VALID_DATA,commonLog);
+//            // }catch(Exception e){
+//            //     LOG.info(e.toString());
+//            //     processContext.output(INVALID_DATA,json);
+//            // }
+//            String[] arrJson=json.split(",");
+//            if(arrJson.length==3) {
+//                //validatios
+//                if(arrJson[0].contains("id") && arrJson[1].contains("name") &&arrJson[2].contains("surname")){
+//                    processContext.output(VALID_Messages,json);
+//                }else{
+//                    //Malformed data
+//                    processContext.output(INVALID_Messages,json);
+//                }
+//            }else{
+//                //Malformed data
+//                processContext.output(INVALID_Messages,json);
+//            }
+//        }
+//    }
 
-            context.output(tableRow);
+//    static public class ConvertToTableRow extends DoFn<CommonLog, TableRow> {
+//        @ProcessElement
+//        public void processing(ProcessContext context) {
+//            TableRow tableRow = new TableRow()
+//                    .set("id", context.element())
+//                    .set("name", context.element())
+//                    .set("surname", context.element());
+//
+//            context.output(tableRow);
+//        }
+//    }
+
+    static public class ConvertToRow extends DoFn<CommonLog, Row> {
+        @ProcessElement
+        public void processing(@Element CommonLog record, OutputReceiver<Row> r) {
+            Row row = Row.withSchema(BQSchema).build();
+            r.output(row);
         }
     }
+
 
     static public void runPipeline(MyOptions options, TableReference tableSpec) {
         Pipeline p = Pipeline.create(options);
@@ -93,17 +129,17 @@ public class MyPipeline {
 
         PCollectionTuple pubSubMessages =
                 p.apply("ReadPubSubMessages", PubsubIO.readStrings()
-                                .withTimestampAttribute("timestamp")
                                 .fromSubscription(options.getInputSubscriptionName()))
-                        .apply("ConvertMessageToCommonLog", new CommonLog());
+                        .apply("ConvertMessageToCommonLog", new JsonToCommonLog());
 
         pubSubMessages
                 .get(VALID_Messages)
-                .setRowSchema(BQSchema)
 
-                .apply("ConvertToTableRow", ParDo.of(new ConvertToTableRow()))
+//                .apply("ConvertToTableRow", ParDo.of(new ConvertToTableRow()))
 
-                .apply("WriteToBQ", BigQueryIO.writeTableRows().to(tableSpec)
+                .apply("ConvertToRow", ParDo.of(new ConvertToRow()))
+
+                .apply("WriteToBQ", BigQueryIO.<Row>write().to(tableSpec)
                                         .withCreateDisposition(BigQueryIO.Write.CreateDisposition.CREATE_NEVER)
                                         .withWriteDisposition(BigQueryIO.Write.WriteDisposition.WRITE_APPEND)
                                 );
